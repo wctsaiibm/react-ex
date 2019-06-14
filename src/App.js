fe895ef3aeab2af3e543836420b0bd652b1ed0de
charles <charlestsai@tw.ibm.com>
2019-06-14 11:15:01 +0800

    try tabs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tabs, Tab } from 'react-bootstrap';
 
 function App() {
+  // TODO: move tab line to top
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tabs defaultActiveKey="1" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Home">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Tab>
+          <Tab eventKey="1" title="1">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          </Tab>
+        </Tabs>
         <a
           className="App-link"
           href="https://reactjs.org"
